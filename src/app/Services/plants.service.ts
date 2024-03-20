38e0aeef89d6871c784130189ac2a07bfbba879e
David Alvarado <DaveLomelitech@gmail.com>
2024-03-20 19:27:45 -0400

    Up to Date

--- src/app/Services/plants.service.ts
+++ src/app/Services/plants.service.ts
@@ -11,12 +11,15 @@ import { TrefleInfo } from '../interfaces/trefle-info';
 export class PlantsService {
 
   constructor(private httpClient: HttpClient) { }
-
+  backEnd = `${environment.apiDomain}/api/Plants`;
   baseURL = `$https://trefle.io/api/v1/plants`;
 
   getTrefleInfo(id: number): Observable<TrefleInfo> {
     return this.httpClient.get<TrefleInfo>(`${this.baseURL}/${id}/?token=-mwv_gdmHkcI3oEhRqEqrXqFAmPdwBmuQf3TqddpT3Y.json`);
 
     // The API says this returns a JSON, but putting JSON in the URL breaks it: https://docs.trefle.io/reference#tag/Plants/operation/listPlants
+  }
+  getPlants(){
+    return this.httpClient.get<Plant[]>(this.backEnd)
   }
 }
