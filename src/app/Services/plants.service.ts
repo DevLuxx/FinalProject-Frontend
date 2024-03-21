d596ecb5820e45f70cb3c7fc8df215abff310859
Frank Wedlick <jfwedlick@gmail.com>
2024-03-20 20:25:54 -0400

    more services

--- src/app/Services/plants.service.ts
+++ src/app/Services/plants.service.ts
@@ -14,38 +14,94 @@ export class PlantsService {
 
   constructor(private httpClient: HttpClient) { }
   plantsAPI = `${environment.apiDomain}/api/Plants`;
-  plotsAPI = `${environment.apiDomain}/api/plots`;
+  plot1API = `${environment.apiDomain}/api/plot1`;
+  plot2API = `${environment.apiDomain}/api/plot2`;
+  plot3API = `${environment.apiDomain}/api/plot3`;
   // trefleAPI = `${environment.apiDomain}/api/trefle`;
 
   getPlants(){
     return this.httpClient.get<Plant[]>(this.plantsAPI)
   }
 
-  getPlots() {
+  getPlot1all() {
     // httpClient.get means this method will be making an http GET request!
-    return this.httpClient.get<Plot[]>(this.plotsAPI)
+    return this.httpClient.get<Plot[]>(this.plot1API)
   }
 
+    // gets plot by id, might not be needed...
+    getPlot1(id: number): Observable<Plot> {
+      return this.httpClient.get<Plot>(`${this.plot1API}/${id}`);
+    }
+
   // this is only used when you want to update the NAME property of a plot. (if you dont add a change name button, then feel free to not use this)
-  updatePlot(updatePlot: Plot, id: number | null) {
+  updatePlot1(updatePlot: Plot, id: number | null) {
     // httpClient.get means this method will be making an http GET request!
-    return this.httpClient.put(`${this.plotsAPI}/${id}`, updatePlot);
+    return this.httpClient.put(`${this.plot1API}/${id}`, updatePlot);
   }
 
   // when a user clicks the "delete plot" button.
-  deletePlot(id: number) {
+  deletePlot1(id: number) {
     // httpClient.get means this method will be making an http GET request!
-    return this.httpClient.delete(`${this.plotsAPI}/${id}`)
+    return this.httpClient.delete(`${this.plot1API}/${id}`)
   }
 
   // this is when a user clicks the "add plot" button
-  addPlot(newPlot: Plot): Observable<Plot> {
-    return this.httpClient.post<Plot>(this.plotsAPI, newPlot);
+  addPlot1(newPlot: Plot): Observable<Plot> {
+    return this.httpClient.post<Plot>(this.plot1API, newPlot);
   }
 
-  // gets plot by id, might not be needed...
-  getPlot(id: number): Observable<Plot> {
-    return this.httpClient.get<Plot>(`${this.plotsAPI}/${id}`);
+  getPlot2all() {
+    // httpClient.get means this method will be making an http GET request!
+    return this.httpClient.get<Plot[]>(this.plot2API)
+  }
+
+    // gets plot by id, might not be needed...
+    getPlot2(id: number): Observable<Plot> {
+      return this.httpClient.get<Plot>(`${this.plot2API}/${id}`);
+    }
+
+  // this is only used when you want to update the NAME property of a plot. (if you dont add a change name button, then feel free to not use this)
+  updatePlot2(updatePlot: Plot, id: number | null) {
+    // httpClient.get means this method will be making an http GET request!
+    return this.httpClient.put(`${this.plot2API}/${id}`, updatePlot);
+  }
+
+  // when a user clicks the "delete plot" button.
+  deletePlot2(id: number) {
+    // httpClient.get means this method will be making an http GET request!
+    return this.httpClient.delete(`${this.plot2API}/${id}`)
+  }
+
+  // this is when a user clicks the "add plot" button
+  addPlot2(newPlot: Plot): Observable<Plot> {
+    return this.httpClient.post<Plot>(this.plot2API, newPlot);
+  }
+
+  getPlot3all() {
+    // httpClient.get means this method will be making an http GET request!
+    return this.httpClient.get<Plot[]>(this.plot3API)
+  }
+
+    // gets plot by id, might not be needed...
+    getPlot3(id: number): Observable<Plot> {
+      return this.httpClient.get<Plot>(`${this.plot3API}/${id}`);
+    }
+
+  // this is only used when you want to update the NAME property of a plot. (if you dont add a change name button, then feel free to not use this)
+  updatePlot3(updatePlot: Plot, id: number | null) {
+    // httpClient.get means this method will be making an http GET request!
+    return this.httpClient.put(`${this.plot3API}/${id}`, updatePlot);
+  }
+
+  // when a user clicks the "delete plot" button.
+  deletePlot3(id: number) {
+    // httpClient.get means this method will be making an http GET request!
+    return this.httpClient.delete(`${this.plot3API}/${id}`)
+  }
+
+  // this is when a user clicks the "add plot" button
+  addPlot3(newPlot: Plot): Observable<Plot> {
+    return this.httpClient.post<Plot>(this.plot3API, newPlot);
   }
 
 }
