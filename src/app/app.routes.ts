10cb2f525637f687abe33913f2f1cba5b199ae26
Nate Cook <stillasweare@yahoo.com>
2024-03-27 15:37:49 -0400

    About Page andbeginning CSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { PlantsComponent } from './plants/plants.component';
 import { DisplayPlotsComponent } from './display-plots/display-plots.component';
 import { JournalComponent } from './journal/journal.component';
 import { PlotDetailComponent } from './plot-detail/plot-detail.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     { path: 'plants', component: PlantsComponent },
     { path: 'plots', component: DisplayPlotsComponent },
     { path: 'journal', component: JournalComponent },
+    { path: 'about', component: AboutComponent },
     {
         path: 'plotitem/:id',
         component: PlotDetailComponent
